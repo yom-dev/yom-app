0e448c0a0f02eb051622fa71adc210deea92b206
jose0229 <sanghoonim@hanyang.ac.kr>
2024-07-24 20:40:18 +0900

    [fix] maincard 컴포넌트 너비 및 높이 %로 설정

--- components/MainCard/MainCard.tsx
+++ components/MainCard/MainCard.tsx
@@ -4,14 +4,14 @@ import React from "react";
 
 const MainCard = () => {
   return (
-    <View className="bg-yomBlack w-full h-full border-none rounded-2xl overflow-hidden">
+    <View className="bg-yomBlack w-full h-full border-none rounded-2xl overflow-hidden ">
       <ImageBackground
         source={require("@/assets/images/main-card-background.png")}
-        className="w-full h-full"
+        className="w-full h-full flex justify-center items-center"
         resizeMode="cover"
         style={{ borderRadius: 20 }} // ImageBackground에 직접 borderRadius 추가
       >
-        <View className="w-full h-full justify-between px-[30px] pt-[32px] pb-[36px]">
+        <View className="w-[85%] h-[85%] flex justify-between">
           <View className="flex flex-column justify-between gap-[5px] h-[20%]">
             <Text className="text-yomWhite font-[WantedM] text-[16px]">
               오늘 하루 감사한 일들을 작성하세요.{" "}
@@ -29,14 +29,16 @@ const MainCard = () => {
               감사하셨나요?
             </Text>
           </View>
-          <View className="h-[40px] w-full">
-            <CustomButton
-              title="작성하기"
-              titleSize={14}
-              backgroundColor="yomWhite"
-              activeBackgroundColor="yomLightGray"
-              textColor="yomGreen"
-            />
+          <View className="h-[20%] flex justify-end">
+            <View className="h-[68%] w-full">
+              <CustomButton
+                title="작성하기"
+                titleSize={14}
+                backgroundColor="yomWhite"
+                activeBackgroundColor="yomLightGray"
+                textColor="yomGreen"
+              />
+            </View>
           </View>
         </View>
       </ImageBackground>
