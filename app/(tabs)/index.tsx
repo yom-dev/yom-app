1aa82e89f84578738d2a777e984528be2d77727c
jose0229 <sanghoonim@hanyang.ac.kr>
2024-07-24 20:16:15 +0900

    [fix] 홈 화면 너비 %로 수정

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,21 +8,23 @@ import MainMyPlan from "@/components/MainMyPlan/MainMyPlan";
 
 export default function HomeScreen() {
   return (
-    <View className="bg-yomWhite w-full flex h-full px-[20px]">
-      <View className="h-[8%] flex flex-col justify-end">
-        <Text className="text-[32px] text-yomBlack font-[WantedSB]">
-          상훈님, 반가워요.
-        </Text>
-      </View>
-
-      <View className="w-full h-[55%] flex flex-col justify-end">
-        <View className="h-[95%]">
-          <MainCard />
+    <View className="h-full w-full bg-yomWhite flex items-center">
+      <View className="bg-yomWhite w-[90%] flex h-full ">
+        <View className="h-[8%] flex flex-col justify-end">
+          <Text className="text-[32px] text-yomBlack font-[WantedSB]">
+            상훈님, 반가워요.
+          </Text>
         </View>
-      </View>
 
-      <View className="w-full h-[37%] ">
-        <MainMyPlan />
+        <View className="w-full h-[55%] flex flex-col justify-end">
+          <View className="h-[95%]">
+            <MainCard />
+          </View>
+        </View>
+
+        <View className="w-full h-[37%] ">
+          <MainMyPlan />
+        </View>
       </View>
     </View>
   );
