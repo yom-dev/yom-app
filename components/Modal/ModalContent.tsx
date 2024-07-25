656737534dff76fbe2f25e33512a3ed28e20268d
jose0229 <sanghoonim@hanyang.ac.kr>
2024-07-26 03:21:13 +0900

    [fix] 모달창에 keyboardAvoidView사용함

--- components/Modal/ModalContent.tsx
+++ components/Modal/ModalContent.tsx
@@ -1,32 +1,41 @@
-import { View, Text, TextInput } from "react-native";
+import { KeyboardAvoidingView, View, Text, TextInput } from "react-native";
 import React from "react";
 
 const ModalContent = () => {
   return (
-    <View className="w-full h-full flex justify-between ">
-      <View className="w-full h-[60px]">
-        <TextInput
-          className="w-full h-full border-yomGray border-[1px] rounded-3xl p-[15px] font-[WantedSB] text-[14px]"
-          placeholder="감사한 일을 적어주세요"
-          keyboardType="default"
-        />
-      </View>
+    <KeyboardAvoidingView
+      className="flex-1"
+      behavior="padding"
+      keyboardVerticalOffset={100}
+    >
+      <View className="w-full h-full flex justify-between ">
+        <View className="w-full h-[60px]">
+          <TextInput
+            className="w-full h-full border-yomGray border-[1px] rounded-3xl p-[15px] font-[WantedSB] text-[14px]"
+            placeholder="이곳에 적어주세요"
+            keyboardType="default"
+            placeholderTextColor={"gray"}
+          />
+        </View>
 
-      <View className="w-full h-[60px]">
-        <TextInput
-          className="w-full h-full border-yomGray border-[1px] rounded-3xl p-[15px] font-[WantedSB] text-[14px]"
-          placeholder="감사한 일을 적어주세요"
-          keyboardType="default"
-        />
+        <View className="w-full h-[60px]">
+          <TextInput
+            className="w-full h-full border-yomGray border-[1px] rounded-3xl p-[15px] font-[WantedSB] text-[14px]"
+            placeholder="이곳에 적어주세요"
+            keyboardType="default"
+            placeholderTextColor={"gray"}
+          />
+        </View>
+        <View className="w-full h-[60px]">
+          <TextInput
+            className="w-full h-full border-yomGray border-[1px] rounded-3xl p-[15px] font-[WantedSB] text-[14px]"
+            placeholder="이곳에 적어주세요"
+            keyboardType="default"
+            placeholderTextColor={"gray"}
+          />
+        </View>
       </View>
-      <View className="w-full h-[60px]">
-        <TextInput
-          className="w-full h-full border-yomGray border-[1px] rounded-3xl p-[15px] font-[WantedSB] text-[14px]"
-          placeholder="감사한 일을 적어주세요"
-          keyboardType="default"
-        />
-      </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
