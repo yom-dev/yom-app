6d2b6f11b72ccf1fcc45645421cf8cc0fcd42c0a
jose0229 <sanghoonim@hanyang.ac.kr>
2024-09-05 22:08:31 +0900

    [fix] planCardwide 클릭 시 라우팅 작업

--- components/PlanCard/PlanCardWide.tsx
+++ components/PlanCard/PlanCardWide.tsx
@@ -1,33 +1,55 @@
 import { View, Text, ImageBackground, TouchableOpacity } from "react-native";
+
 import { LinearGradient } from "expo-linear-gradient";
+
 import { planGradation } from "@/constants/planGradation";
+
 import { icons } from "@/constants/Icons";
+
 import { Ionicons } from "@expo/vector-icons";
+
 import Entypo from "@expo/vector-icons/Entypo";
+
+import { router } from "expo-router";
+
 type IconPlanName = keyof typeof icons;
+
 type GradationPlanName = keyof typeof planGradation;
 
 const PlanCard = ({
   planName,
+
   title,
+
   subTitle,
 }: {
   planName: string;
+
   title: string;
+
   subTitle: string;
 }) => {
   const GradationPlanName = planName as GradationPlanName;
+
   const IconPlanName = planName as IconPlanName;
 
   const { startColor, endColor } = planGradation[
     IconPlanName as keyof typeof planGradation
   ] || {
     startColor: "#000",
+
     endColor: "#FFF",
   };
 
+  const handlePress = () => {
+    router.push(`/plan/plan/${planName}`);
+  };
+
   return (
-    <TouchableOpacity className="w-full h-[80px] rounded-xl">
+    <TouchableOpacity
+      className="w-full h-[80px] rounded-xl"
+      onPress={handlePress}
+    >
       <LinearGradient
         colors={[startColor, endColor]}
         start={[1, 0]}
@@ -42,14 +64,17 @@ const PlanCard = ({
               resizeMode="contain"
             />
           </View>
+
           <View className=" w-[60%] h-full flex justify-between">
             <Text className="font-[WantedB] text-[20px] text-white">
               {title}
             </Text>
+
             <Text className="font-[WantedM] text-[12px] text-white opacity-1">
               {subTitle}
             </Text>
           </View>
+
           <View className="justify-end flex-row w-[20%]">
             <Entypo name="chevron-thin-right" size={24} color="white" />
           </View>
