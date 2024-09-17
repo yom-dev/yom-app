4e62b61dbb9aef8c100703523af5e3daa16eecfe
jose0229 <sanghoonim@hanyang.ac.kr>
2024-09-17 23:50:35 +0900

    [fix] useGetMyPlans 에러 해결

--- hooks/useGetMyPlans.ts
+++ hooks/useGetMyPlans.ts
@@ -63,12 +63,3 @@ const useGetMyPlans = () => {
 };
 
 export default useGetMyPlans;
-
-// MyPlans 타입 정의
-export type MyPlans = {
-  id: string;
-  reading: boolean;
-  gratitude: boolean;
-  meditation: boolean;
-  running: boolean;
-};
